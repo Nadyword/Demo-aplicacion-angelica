53b0570cd5edc28e80a0a3353922a372b581b1fd
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-06-05 16:21:33 -0400

    Ultimos detalles

--- obj/Debug/net8.0-windows/HitoriaClinica.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/HitoriaClinica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HitoriaClinica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba45d9acb19267df78e13deecabeda9daf72bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece9ce95af2b2717f3fd12f6c49a88208a688530")]
 [assembly: System.Reflection.AssemblyProductAttribute("HitoriaClinica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HitoriaClinica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
